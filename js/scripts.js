8348cf7331024742ff53cf19f4b4ebf8c7866eba
LeviDPS <levi@theviewfromhere.com.au>
2016-07-05 14:04:44 +1000

    added ajax page loading

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
+// Ajax page load/history update/animation function
+function pageLoad() {
+
+    var siteUrl = 'http://'+(document.location.hostname||document.location.host);
+
+    //  Catch all internally-focused links and push a new state.
+    //  Note: External links will not be affected by this behavior.
+    $(document).delegate('a[href^="/"],a[href^="'+siteUrl+'"]', "click", function(e) {
+        e.preventDefault();
+        History.pushState({}, "", this.pathname);
+    });
+
+    History.Adapter.bind(window, 'statechange', function(){
+        var State = History.getState();
+        $.get(State.url, function(data){    // Use AJAX to get the new content.
+            document.title = data.match(/<title>(.*?)<\/title>/)[1];
+            $('html,body').animate({scrollTop:0}, '600', 'swing');
+            $('.content').fadeOut(600).queue( function() {
+                $(this).html( $(data).find('.content') ).dequeue();
+            }).fadeIn(500); // Pull the post we want out of the .content class.
+
+                                                            // If you change the class of the post container,
+                                                            // you must change it here!!!
+            _gaq.push(['_trackPageview', State.url]);   // This updates Google Analytics with a visit to the new page.
+                                                        // If you don't use Google Analytics, you can safety comment or
+                                                        // remove that line.
+
+            transition();
+
+            $('.face h2').html(greet[t]);
+            $('.face p').html(text[t]);
+
+        });
+    });
+
+}
+
 
 // Header logo (display social links )
 function social() {
@@ -33,16 +70,18 @@ function transition() {
          i++;
     }
 
+    $('.face h2').html(greet[t]);
+    $('.face p').html(text[t]);
+
 }
 
 
 // Init scripts
 $(document).ready(function() {
 
+    pageLoad();
     social();
     about();
-    $('.face h2').html(greet[t]);
-    $('.face p').html(text[t]);
 
     setInterval(transition, 550);
 
