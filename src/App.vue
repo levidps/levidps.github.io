70adb8964354980f5c71b959fe464b6b438fb331
Levidps <levidps@gmail.com>
2024-07-31 23:56:48 +1000

    initial commit of new vue based site

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1>levidps</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  display: none;
+}
+</style>
